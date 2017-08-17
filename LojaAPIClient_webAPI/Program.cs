8d437591d68acb1d83ed72d15657151f86fabadc
Rafael Bertelli <rafaelbertelli89@gmail.com>
2017-08-16 22:47:50 -0300

    URI to XML

--- LojaAPIClient_webAPI/Program.cs
+++ LojaAPIClient_webAPI/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,19 @@ namespace LojaAPIClient_webAPI
     {
         static void Main(string[] args)
         {
+            string conteudo;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("http://www.mocky.io/v2/52aaf5deee7ba8c70329fb7d");
+
+            WebResponse response = request.GetResponse();
+            using(Stream responseStream = response.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(responseStream, Encoding.UTF8);
+                conteudo = reader.ReadToEnd();
+            }
+
+            Console.Write(conteudo);
+            Console.Read();
+
         }
     }
 }
